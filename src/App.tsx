208a98d2e92aab010ac00e5395906f7443d51c71
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-01 19:33:41 +0300

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Header from "./components/Header/Header";
+import './scss/app.scss';
+import Categories from "./components/Categories/Categories";
+import Sort from "./components/Sort/Sort";
+import Catalog, {PizzaType} from "./components/Catalog/Catalog";
 
 function App() {
+
+    let [pizza, setPizza] = useState<Array<PizzaType>>([
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" },
+        { title: "Мексиканская" }
+    ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="wrapper">
+          <Header />
+          <div className="content">
+              <div className="container">
+                  <div className="content__top">
+                      <Categories />
+                      <Sort />
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                      <Catalog title="Мексиканская" price={500} />
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                      <Catalog title="Мексиканская" price={500}/>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
