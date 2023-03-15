f8b5ebc38dfd03bad8c7f37465404b12b94a1e13
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-15 18:22:55 +0300

    adding a function and conditions for category selection

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -3,15 +3,38 @@ import React, {useState} from "react";
 function Categories() {
     const [selectedCategory, setSelectedCategory] = useState(0);
 
+    const onClickCategory = (id: number) => {
+        setSelectedCategory(id);
+    }
+
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li onClick={() => onClickCategory(0)}
+                    className={selectedCategory === 0 ? "active" : ""}>
+                    Все
+                </li>
+                <li onClick={() => onClickCategory(1)}
+                    className={selectedCategory === 1 ? "active" : ""}>
+                    Мясные
+                </li>
+                <li onClick={() => onClickCategory(2)}
+                    className={selectedCategory === 2 ? "active" : ""}>
+                    Вегетарианская
+                </li>
+                <li onClick={() => onClickCategory(3)}
+                    className={selectedCategory === 3 ? "active" : ""}>
+                    Гриль
+                </li>
+                <li onClick={() => onClickCategory(4)}
+                    className={selectedCategory === 4 ? "active" : ""}>
+                    Острые
+                </li>
+                <li onClick={() => onClickCategory(5)}
+                    className={selectedCategory === 5 ? "active" : ""}>
+                    Закрытые
+                </li>
             </ul>
         </div>
     )
