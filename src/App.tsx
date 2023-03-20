0c4f20e584d5f56a268fde5ed3ba133ef0ebee9e
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-20 09:30:16 +0300

    transferring logic to a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -5,51 +5,16 @@ import Categories from "./components/Categories/Categories";
 import Sort from "./components/Sort/Sort";
 import Catalog from "./components/Catalog/Catalog";
 import Placeholder from "./components/Placeholder/Placeholder";
-
-type itemsData = {
-    title: string,
-    price: number,
-    imageUrl: string,
-    sizes: Array<number>,
-    types: Array<number>
-}
+import Home from "./components/Home/Home";
 
 function App() {
-    const [items, setItems] = useState<Array<itemsData>>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-
-    useEffect(() => {
-        fetch("https://64145f1f9172235b8692eea8.mockapi.io/items")
-            .then((res) => {
-                return res.json();
-            })
-            .then((arr) => {
-                setItems(arr);
-                setIsLoading(false);
-            });
-    }, []);
 
     return (
       <div className="wrapper">
           <Header />
           <div className="content">
               <div className="container">
-                  <div className="content__top">
-                      <Categories />
-                      <Sort />
-                  </div>
-                  <h2 className="content__title">Все пиццы</h2>
-                  <div className="content__items">
-                      {
-                          isLoading ? [...new Array(6)].map((_, index) => <Placeholder key={index} />)
-                              : items.map(pizza => <Catalog key={pizza.title}
-                                                            title={pizza.title}
-                                                            price={pizza.price}
-                                                            imagePizza={pizza.imageUrl}
-                                                            sizes={pizza.sizes}
-                                                            types={pizza.types} />)
-                      }
-                  </div>
+                  <Home />
               </div>
           </div>
       </div>
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,52 @@
+import Categories from "../Categories/Categories";
+import Sort from "../Sort/Sort";
+import Placeholder from "../Placeholder/Placeholder";
+import Catalog from "../Catalog/Catalog";
+import React, {useEffect, useState} from "react";
+
+type itemsData = {
+    title: string,
+    price: number,
+    imageUrl: string,
+    sizes: Array<number>,
+    types: Array<number>
+}
+
+function Home() {
+    const [items, setItems] = useState<Array<itemsData>>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        fetch("https://64145f1f9172235b8692eea8.mockapi.io/items")
+            .then((res) => {
+                return res.json();
+            })
+            .then((arr) => {
+                setItems(arr);
+                setIsLoading(false);
+            });
+    }, []);
+
+    return (
+        <>
+            <div className="content__top">
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {
+                    isLoading ? [...new Array(6)].map((_, index) => <Placeholder key={index} />)
+                        : items.map(pizza => <Catalog key={pizza.title}
+                                                      title={pizza.title}
+                                                      price={pizza.price}
+                                                      imagePizza={pizza.imageUrl}
+                                                      sizes={pizza.sizes}
+                                                      types={pizza.types} />)
+                }
+            </div>
+        </>
+    )
+}
+
+export default Home;
