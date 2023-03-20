a09da6986bc653631742444a9b65b653b027f6c5
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-20 09:31:12 +0300

    removal of excess

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import Header from "./components/Header/Header";
 import './scss/app.scss';
-import Categories from "./components/Categories/Categories";
-import Sort from "./components/Sort/Sort";
-import Catalog from "./components/Catalog/Catalog";
-import Placeholder from "./components/Placeholder/Placeholder";
 import Home from "./components/Home/Home";
 
 function App() {
