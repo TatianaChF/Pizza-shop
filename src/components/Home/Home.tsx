b1f34658546dbef1cc47faff4cb92f360913e0cc
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-21 09:18:06 +0300

    passing categoryId to a category component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -32,7 +32,7 @@ function Home() {
     return (
         <>
             <div className="content__top">
-                <Categories />
+                <Categories categoryId={categoryId} />
                 <Sort />
             </div>
             <h2 className="content__title">Все пиццы</h2>
