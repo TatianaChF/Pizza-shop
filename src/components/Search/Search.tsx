52fe5ab49f2127b43effe5561d93656afc8c0d15
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-25 12:55:23 +0300

    add search icon

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,7 +1,9 @@
 import style from "./Search.module.scss";
+import searchIcon from "../../assets/img/search.svg";
 function Search() {
     return (
         <div>
+            <img src={searchIcon} />
             <input className={style.root} placeholder="Найти пиццу..." />
         </div>
     )
