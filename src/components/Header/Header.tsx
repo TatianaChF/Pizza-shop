ad23a3205d7668fc66cc0974624bbc77c084b5f8
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-25 12:33:12 +0300

    import search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import logo from "../../assets/img/pizza-logo.svg";
 import React from "react";
 import {Link} from "react-router-dom";
+import Search from "../Search/Search";
 function Header() {
     return (
         <div className="header">
@@ -14,6 +15,7 @@ function Header() {
                         </div>
                     </div>
                 </Link>
+                <Search />
                 <div className="header__cart">
                     <Link to="/cart" className="button button--cart">
                         <span>520 ₽</span>
