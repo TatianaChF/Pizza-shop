f58a910b6daeea3ed8411d3ec2477ba6f4ebad64
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-27 09:56:47 +0300

    change input value and searchValue

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,11 +1,14 @@
 import style from "./Search.module.scss";
 import searchIcon from "../../assets/img/search.svg";
 import {PropsTypeSearch} from "../Header/Header";
+import {ChangeEvent} from "react";
 function Search( props: PropsTypeSearch ) {
     return (
         <div className={style.root} >
             <img src={searchIcon} className={style.icon} />
-            <input className={style.input} placeholder="Найти пиццу..." />
+            <input onChange={(event: ChangeEvent<HTMLInputElement>) => props.setSearchValue(event.target.value) }
+                className={style.input}
+                placeholder="Найти пиццу..." />
         </div>
     )
 }
