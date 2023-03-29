28388bff1a7f29a37bbf8fce26108391ee080ed0
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-29 20:52:38 +0300

    creating a search context

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,23 @@ import NotFound from "./components/NotFound/NotFound";
 import {Route, Routes} from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 
-const SearchContext = React.createContext("");
+const SearchContext = React.createContext({});
 
 function App() {
     const [searchValue, setSearchValue] = useState("");
 
     return (
       <div className="wrapper">
-          <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-          <div className="content">
+          <SearchContext.Provider value={{searchValue, setSearchValue}}>
+              <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+              <div className="content">
                   <Routes>
                       <Route path="/" element={<Home searchValue={searchValue} setSearchValue={setSearchValue} />} />
                       <Route path="/cart" element={<Cart />} />
                       <Route path="*" element={<NotFound />} />
                   </Routes>
-          </div>
+              </div>
+          </SearchContext.Provider>
       </div>
   );
 }
