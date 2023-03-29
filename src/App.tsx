95656edad090bb301534a52a7af8b29c23b918a5
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-29 20:50:40 +0300

    creating a search context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {useState} from 'react';
 import Header from "./components/Header/Header";
 import './scss/app.scss';
 import Home from "./components/Home/Home";
 import NotFound from "./components/NotFound/NotFound";
 import {Route, Routes} from "react-router-dom";
 import Cart from "./components/Cart/Cart";
+
+const SearchContext = React.createContext("");
 
 function App() {
     const [searchValue, setSearchValue] = useState("");
