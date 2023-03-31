3ca548f098125bda14b1fd99aeed1de2b5656323
TatyanaChekina <tatiana.chefox@gmail.com>
2023-03-31 14:17:47 +0300

    creating a method for changing the category id

--- src/redux/slices/filterSlice.ts
+++ src/redux/slices/filterSlice.ts
@@ -18,21 +18,15 @@ const initialState: FilterState = {
 }
 
 export const filterSlice = createSlice({
-    name: 'filter',
+    name: 'filters',
     initialState,
     reducers: {
-        increment: (state) => {
-            state.value += 1
-        },
-        decrement: (state) => {
-            state.value -= 1
-        },
-        incrementByAmount: (state, action: PayloadAction<number>) => {
-            state.value += action.payload
-        },
-    },
+        setCategoryId: (state, action) => {
+            state.categoryID = action.payload
+        }
+    }
 })
 
-export const { increment, decrement, incrementByAmount } = filterSlice.actions
+export const { setCategoryId } = filterSlice.actions
 
 export default filterSlice.reducer
