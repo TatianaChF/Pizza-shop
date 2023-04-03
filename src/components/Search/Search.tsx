45465fd71cd522df38dce1077a83c81cda50c731
TatyanaChekina <tatiana.chefox@gmail.com>
2023-04-03 09:58:36 +0300

    creating function that runs on a timer

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,15 +1,29 @@
 import style from "./Search.module.scss";
 import searchIcon from "../../assets/img/search.svg";
-import {ChangeEvent, useContext, useRef} from "react";
+import {ChangeEvent, useCallback, useContext, useRef} from "react";
 import closeIcon from "../../assets/img/close.svg";
 import {SearchContext} from "../../App";
+import debounce from "lodash.debounce";
 
 function Search() {
     const {searchValue, setSearchValue} = useContext(SearchContext);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const requestTimer  = useCallback(
+        debounce(() => {
+            console.log("Request");
+        }, 1000),
+        []
+    );
+
     const onClickClear = () => {
         setSearchValue("");
         if (inputRef.current) inputRef.current.focus();
+    }
+
+    const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(event.target.value);
+        requestTimer();
     }
 
     return (
@@ -18,7 +32,7 @@ function Search() {
             <input
                 ref={inputRef}
                 value={searchValue}
-                onChange={(event: ChangeEvent<HTMLInputElement>) => setSearchValue(event.target.value) }
+                onChange={ onChangeInput }
                 className={style.input}
                 placeholder="Найти пиццу..." />
             {searchValue && (
