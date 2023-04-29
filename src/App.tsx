4446806aa36b8ab6d240cd589828b0613f3a8c34
TatyanaChekina <tatiana.chefox@gmail.com>
2023-04-29 11:49:24 +0300

    removing context

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {SetStateAction, useState} from 'react';
+import React from 'react';
 import Header from "./components/Header/Header";
 import './scss/app.scss';
 import Home from "./components/Home/Home";
@@ -6,33 +6,19 @@ import NotFound from "./components/NotFound/NotFound";
 import {Route, Routes} from "react-router-dom";
 import Cart from "./components/Cart/Cart";
 
-type SearchValues = {
-    searchValue: string,
-    setSearchValue: React.Dispatch<React.SetStateAction<string>>
-}
-
-export const SearchContext = React.createContext<SearchValues>({
-    searchValue: "",
-    setSearchValue: (value: SetStateAction<string>) => value
-});
-
 function App() {
-    const [searchValue, setSearchValue] = useState("");
-
     return (
-      <div className="wrapper">
-          <SearchContext.Provider value={{searchValue, setSearchValue}}>
-              <Header />
-              <div className="content">
-                  <Routes>
-                      <Route path="/" element={<Home />} />
-                      <Route path="/cart" element={<Cart />} />
-                      <Route path="*" element={<NotFound />} />
-                  </Routes>
-              </div>
-          </SearchContext.Provider>
-      </div>
-  );
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home/>}/>
+                    <Route path="/cart" element={<Cart/>}/>
+                    <Route path="*" element={<NotFound/>}/>
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
