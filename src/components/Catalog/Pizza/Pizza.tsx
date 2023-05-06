801bd01aa9f20fba636a2bcdfc4ffae3095d3a1d
TatyanaChekina <tatiana.chefox@gmail.com>
2023-05-06 12:16:36 +0300

    creating a function to get pizza

--- src/components/Catalog/Pizza/Pizza.tsx
+++ src/components/Catalog/Pizza/Pizza.tsx
@@ -1,12 +1,20 @@
 import {useParams} from "react-router-dom";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 function Pizza() {
+    const [pizza, setPizza] = useState();
     const { id } = useParams();
     
     useEffect(() => {
-        axios.get("https://64145f1f9172235b8692eea8.mockapi.io/items" + id)
+        async function fetchPizza() {
+            try {
+                const { data } = await axios.get("https://64145f1f9172235b8692eea8.mockapi.io/items" + id);
+                setPizza(data);
+            } catch (error) {
+                alert("Ошибка при получении пиццы!");
+            }
+        }
     }, [])
 
     return (
