4ec66c775870575f1ed0f376523f6a57662e34a3
TatyanaChekina <tatiana.chefox@gmail.com>
2023-05-06 10:39:28 +0300

    creating a component template

---
+++ src/components/Catalog/Pizza/Pizza.tsx
@@ -0,0 +1,12 @@
+function Pizza() {
+    return (
+        <div>
+            <img />
+            <h2></h2>
+            <p></p>
+            <h4></h4>
+        </div>
+    )
+}
+
+export default Pizza;
