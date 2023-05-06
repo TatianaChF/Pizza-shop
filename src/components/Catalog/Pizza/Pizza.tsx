2b8322196b21638cb9bab2e23c0dacdbbb067a5b
TatyanaChekina <tatiana.chefox@gmail.com>
2023-05-06 12:36:12 +0300

    output information about pizza

--- src/components/Catalog/Pizza/Pizza.tsx
+++ src/components/Catalog/Pizza/Pizza.tsx
@@ -2,27 +2,41 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 
+type PizzaType = {
+    imageUrl: string,
+    title: string,
+    price: string
+}
+
 function Pizza() {
-    const [pizza, setPizza] = useState();
+    const [pizza, setPizza] = useState<PizzaType>();
     const { id } = useParams();
     
     useEffect(() => {
         async function fetchPizza() {
             try {
-                const { data } = await axios.get("https://64145f1f9172235b8692eea8.mockapi.io/items" + id);
+                const { data } = await axios.get("https://64145f1f9172235b8692eea8.mockapi.io/items/" + id);
                 setPizza(data);
             } catch (error) {
                 alert("Ошибка при получении пиццы!");
             }
         }
+
+        fetchPizza();
     }, [])
+
+    if (!pizza) {
+        return (
+            <div>Загрузка</div>
+        );
+    }
 
     return (
         <div>
-            <img />
-            <h2>{id}</h2>
+            <img src={pizza.imageUrl} />
+            <h2>{pizza.title}</h2>
             <p></p>
-            <h4></h4>
+            <h4>{pizza.price}</h4>
         </div>
     )
 }
