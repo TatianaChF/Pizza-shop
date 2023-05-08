1558256ebb485e3875283319de55668ecc69ca87
TatyanaChekina <tatiana.chefox@gmail.com>
2023-05-08 22:27:49 +0300

    passing a child

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,10 +1,15 @@
 import Header from "../components/Header/Header";
 import React from "react";
 
-function MainLayout() {
+type ChildrenElement = {
+    children: any
+}
+
+function MainLayout( { children }: ChildrenElement ) {
     return (
         <div className="wrapper">
             <Header/>
+            <div className="content">{children}</div>
         </div>
     )
 }
