8358eea9b85b101ccec4d4486ae54b398c97309e
tikaya <tatiana.chefox@gmail.com>
2023-07-04 15:28:44 +0300

    removal of an array of objects

--- src/components/Home/Categories/Categories.tsx
+++ src/components/Home/Categories/Categories.tsx
@@ -6,15 +6,16 @@ type PropsType = {
     onClickCategory: (id: number) => void
 }
 
+const categories = [
+    {id: 0, title: "Все"},
+    {id: 1, title: "Мясные"},
+    {id: 2, title: "Вегетарианская"},
+    {id: 3, title: "Гриль"},
+    {id: 4, title: "Острые"},
+    {id: 5, title: "Закрытые"},
+]
+
 function Categories(props: PropsType) {
-    const categories = [
-        {id: 0, title: "Все"},
-        {id: 1, title: "Мясные"},
-        {id: 2, title: "Вегетарианская"},
-        {id: 3, title: "Гриль"},
-        {id: 4, title: "Острые"},
-        {id: 5, title: "Закрытые"},
-    ]
 
     return (
         <div className={styles.categories}>
