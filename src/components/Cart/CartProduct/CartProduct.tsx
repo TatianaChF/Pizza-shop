831a755f237a69b09021a82fc5d3015bfce2db0f
tikaya <tatiana.chefox@gmail.com>
2023-07-17 18:33:11 +0300

    change styles

--- src/components/Cart/CartProduct/CartProduct.tsx
+++ src/components/Cart/CartProduct/CartProduct.tsx
@@ -52,7 +52,7 @@ function CartProduct(props: CartProductProps) {
             </div>
             <div className={styles.cart__item_count}>
                 <button disabled={props.count === 1} onClick={onClickMinus} 
-                className={props.count === 1 ? styles.button__minus__disabled : styles.button__minus}>
+                className={styles.button__minus}>
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
