c00caf9b2a36bd3a46eb8103570129db1a6c7902
tikaya <tatiana.chefox@gmail.com>
2023-07-18 11:31:01 +0300

    wrapping a function in a hook

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,7 @@ import Categories from "./Categories/Categories";
 import Sort, {sortList} from "./Sort/Sort";
 import Placeholder from "../Placeholder/Placeholder";
 import Catalog from "../Catalog/Catalog";
-import {useEffect, useRef} from "react";
+import {useCallback, useEffect, useRef} from "react";
 import Pagination from "../Pagination/Pagination";
 import {useSelector} from "react-redux";
 import {useAppDispatch} from "../../redux/store";
@@ -76,9 +76,9 @@ function Home() {
         isMounted.current = true;
     }, [categoryId, sortType, pageCount]);
 
-    const onChangeCategory = (id: number) => {
+    const onChangeCategory = useCallback((id: number) => {
         dispatch(setCategoryId(id));
-    }
+    }, []); 
 
     const onChangePage = (page: number) => {
         dispatch(setPageCount(page));
