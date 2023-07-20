4a0e5e22086d1088d21d4ad68365157bbbfee419
tikaya <tatiana.chefox@gmail.com>
2023-07-20 09:50:31 +0300

    adding lazy for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
 import "./App.scss";
 import Home from "./components/Home/Home";
-import NotFound from "./components/NotFound/NotFound";
 import {Route, Routes} from "react-router-dom";
-import Cart from "./components/Cart/Cart";
-import Pizza from "./components/Catalog/Pizza/Pizza";
 import MainLayout from "./layouts/MainLayout";
+import React, { Suspense } from "react";
+
+const Cart = React.lazy(() => import("./components/Cart/Cart"));
+const Pizza = React.lazy(() => import("./components/Catalog/Pizza/Pizza"));
+const NotFound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<MainLayout/>}>
                 <Route path="/" element={<Home/>}/>
-                <Route path="/cart" element={<Cart/>}/>
-                <Route path="/pizza/:id" element={<Pizza/>}/>
-                <Route path="*" element={<NotFound/>}/>
+                <Route path="/cart" element={
+                    <Suspense fallback={<div>Загрузка корзины...</div>}>
+                        <Cart />
+                    </Suspense>
+                }/>
+                <Route path="/pizza/:id" element={
+                    <Suspense fallback={<div>Загрузка пиццы...</div>}>
+                        <Pizza/>
+                    </Suspense>
+                }/>
+                <Route path="*" element={
+                    <Suspense fallback={<div>Загрузка...</div>}>
+                        <NotFound/>
+                    </Suspense>
+                }/>
             </Route>
         </Routes>
     );
