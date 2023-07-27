fcaaffb63c14848081729915b691d97043ac0083
tikaya <tatiana.chefox@gmail.com>
2023-07-27 11:45:38 +0300

    converting a component to an arrow function

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,8 +1,7 @@
 import Header from "../components/Header/Header";
-import React from "react";
 import {Outlet} from "react-router-dom";
 
-function MainLayout() {
+const MainLayout = () => {
     return (
         <div className="wrapper">
             <Header/>
