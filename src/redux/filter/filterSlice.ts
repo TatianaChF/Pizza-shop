23454659abd253aaff530c9309120bf78b1e7c6c
tikaya <tatiana.chefox@gmail.com>
2023-08-03 10:18:38 +0300

    removing redundant imports

--- src/redux/filter/filterSlice.ts
+++ src/redux/filter/filterSlice.ts
@@ -1,5 +1,4 @@
 import {PayloadAction, createSlice} from '@reduxjs/toolkit'
-import {RootState} from "../store";
 import { FilterSort, FilterState } from './types';
 
 const initialState: FilterState = {
