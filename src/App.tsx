1232711aa08dbd15b99fffc1a4ec2c910811fcbb
tikaya <tatiana.chefox@gmail.com>
2023-08-10 10:59:18 +0300

    adding translations to download

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,31 @@ import Home from "./components/Home/Home";
 import {Route, Routes} from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import React, { Suspense } from "react";
+import { useTranslation } from "react-i18next";
 
 const Cart = React.lazy(() => import("./components/Cart/Cart"));
 const Pizza = React.lazy(() => import("./components/Catalog/Pizza/Pizza"));
 const NotFound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 const App = () => {
+    const {t} = useTranslation();
+
     return (
         <Routes>
             <Route path="/" element={<MainLayout/>}>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/cart" element={
-                    <Suspense fallback={<div>Загрузка корзины...</div>}>
+                    <Suspense fallback={<div>{`${t('loading.cartLoading')}`}</div>}>
                         <Cart />
                     </Suspense>
                 }/>
                 <Route path="/pizza/:id" element={
-                    <Suspense fallback={<div>Загрузка пиццы...</div>}>
+                    <Suspense fallback={<div>{`${t('loading.pizzaLoading')}`}</div>}>
                         <Pizza/>
                     </Suspense>
                 }/>
                 <Route path="*" element={
-                    <Suspense fallback={<div>Загрузка...</div>}>
+                    <Suspense fallback={<div>{`${t('loading.loadingMain')}`}</div>}>
                         <NotFound/>
                     </Suspense>
                 }/>
