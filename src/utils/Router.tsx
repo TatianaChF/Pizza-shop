ef5a3aee634423fa165a8ac5fb0e25c9676c3574
tikaya <tatiana.chefox@gmail.com>
2023-08-11 11:41:40 +0300

    replacing MainLayout with App

--- src/utils/Router.tsx
+++ src/utils/Router.tsx
@@ -1,16 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
 import NotFound from "../components/NotFound/NotFound";
 import Home from "../components/Home/Home";
-import ReactDOM from "react-dom";
 import ErrorPage from "../components/ErrorPage/ErrorPage";
 import Cart from "../components/Cart/Cart";
 import Pizza from "../components/Catalog/Pizza/Pizza";
+import App from "../App";
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <MainLayout />,
+        element: <App />,
         errorElement: <ErrorPage />,
         children: [
             {
