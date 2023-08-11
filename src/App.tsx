72a300cb05eacb670d09ab58ec8e347519a52978
tikaya <tatiana.chefox@gmail.com>
2023-08-11 11:40:38 +0300

    moving code from MainLayout

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
 import "./App.scss";
-import Home from "./components/Home/Home";
-import {Route, Routes} from "react-router-dom";
-import MainLayout from "./layouts/MainLayout";
-import React, { Suspense } from "react";
+import {Outlet} from "react-router-dom";
+import React from "react";
 import { useTranslation } from "react-i18next";
+import Header from "./components/Header/Header";
 
 const Cart = React.lazy(() => import("./components/Cart/Cart"));
 const Pizza = React.lazy(() => import("./components/Catalog/Pizza/Pizza"));
 const NotFound = React.lazy(() => import("./components/NotFound/NotFound"));
 
 const App = () => {
-    const {t} = useTranslation();
-
     return (
-        <Routes>
-            <Route path="/" element={<MainLayout/>}>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/cart" element={
-                    <Suspense fallback={<div>{`${t('loading.cartLoading')}`}</div>}>
-                        <Cart />
-                    </Suspense>
-                }/>
-                <Route path="/pizza/:id" element={
-                    <Suspense fallback={<div>{`${t('loading.pizzaLoading')}`}</div>}>
-                        <Pizza/>
-                    </Suspense>
-                }/>
-                <Route path="*" element={
-                    <Suspense fallback={<div>{`${t('loading.loadingMain')}`}</div>}>
-                        <NotFound/>
-                    </Suspense>
-                }/>
-            </Route>
-        </Routes>
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <Outlet/>
+            </div>
+        </div>
     );
 }
 
