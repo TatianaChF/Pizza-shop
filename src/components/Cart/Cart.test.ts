bcd51c3e0bee85fbf69927a46b40e687f60c94e7
tikaya <tatiana.chefox@gmail.com>
2023-08-15 11:11:10 +0300

    import APIs

---
+++ src/components/Cart/Cart.test.ts
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react";
+import {describe, expect, test} from '@jest/globals';
+import Cart from "./Cart";
+
+describe("Cart component", () => {
+    test("calculation of the total amount of the order", () => {
+        
+    })
+})
