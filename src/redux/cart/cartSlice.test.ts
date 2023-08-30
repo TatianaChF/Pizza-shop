c47ccde5b10252c85db47e41d3f062036d2b8d1a
tikaya <tatiana.chefox@gmail.com>
2023-08-30 14:37:00 +0300

    delete import

--- src/redux/cart/cartSlice.test.ts
+++ src/redux/cart/cartSlice.test.ts
@@ -1,4 +1,3 @@
-import "@testing-library/jest-dom/extend-expect"
 import {expect, test} from '@jest/globals';
 import { CartState } from "./types";
 import cartReducer, { addProduct, clearProducts, minusProduct, removeProduct } from './cartSlice';
