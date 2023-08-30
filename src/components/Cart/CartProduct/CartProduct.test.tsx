9f370da7c3e67eab8ca15106266edd7511dc4ab4
tikaya <tatiana.chefox@gmail.com>
2023-08-30 14:52:57 +0300

    adding Provider

---
+++ src/components/Cart/CartProduct/CartProduct.test.tsx
@@ -0,0 +1,24 @@
+import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import CartProduct from "./CartProduct";
+import { toBeInTheDocument } from "@testing-library/jest-dom/matchers";
+import { Provider } from 'react-redux';
+import { store } from '../../../redux/store';
+
+test("renders a title", () => {
+    render(
+        <Provider store={store}>
+           <CartProduct id={1} 
+            title="Сырная" 
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+            price={245}
+            count={1}
+            size={26}
+            type="0" /> 
+        </Provider>
+    );
+    const linkElement = screen.getByRole("Сырная");
+    expect(linkElement),toBeInTheDocument();
+
+})
