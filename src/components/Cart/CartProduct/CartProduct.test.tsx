828f7d3231cce88ac169ee118f54b46129225442
tikaya <tatiana.chefox@gmail.com>
2023-08-31 16:15:04 +0300

    adding tests to check the output of information

--- src/components/Cart/CartProduct/CartProduct.test.tsx
+++ src/components/Cart/CartProduct/CartProduct.test.tsx
@@ -18,7 +18,38 @@ test("renders a title", () => {
             type="0" /> 
         </Provider>
     );
-    const linkElement = screen.getByText("home.title.Сырная");
-    expect(linkElement).toBeInTheDocument();
+    const titleElement = screen.getByText("home.title.Сырная");
+    expect(titleElement).toBeInTheDocument();
+});
 
-})
+test("renders a count", () => {
+    render(
+        <Provider store={store}>
+            <CartProduct id={1} 
+            title="Сырная" 
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+            price={245}
+            count={1}
+            size={26}
+            type="0" /> 
+        </Provider>
+    );
+    const countElement = screen.getByText("1");
+    expect(countElement).toBeInTheDocument();
+});
+
+test("renders a type and a size", () => {
+    render(
+        <Provider store={store}>
+            <CartProduct id={1} 
+            title="Сырная" 
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+            price={245}
+            count={1}
+            size={26}
+            type="thin" /> 
+        </Provider>
+    );
+    const typeElement = screen.getByRole("p");
+    expect(typeElement).toHaveTextContent("home.thickness.thin, 26 home.abbreviations.sm");
+});
