308d209ef7e7dd6dccc55fbe445a0d5d2ae80341
tikaya <tatiana.chefox@gmail.com>
2023-09-14 11:02:35 +0300

    pizza size test

--- src/components/Catalog/Catalog.test.tsx
+++ src/components/Catalog/Catalog.test.tsx
@@ -58,3 +58,21 @@ test("displaying the name of the pizza", () => {
     expect(h4).toBeInTheDocument();
     expect(h4).toHaveTextContent("Пепперони Фреш с перцем");
 })
+
+test("getting pizza sizes", () => {
+    render(
+        <Provider store={store}>
+            <Catalog id={0}
+            title="Пепперони Фреш с перцем"
+            price={803}
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg"
+            count={1}
+            sizes={[26, 30, 40]}
+            types={[0, 1]} />
+        </Provider>
+    );
+
+    const sizes = screen.getByRole("sizes");
+    expect(sizes).toBeInTheDocument();
+    expect(sizes).toHaveTextContent("26 home.abbreviations.sm30 home.abbreviations.sm40 home.abbreviations.sm");
+})
