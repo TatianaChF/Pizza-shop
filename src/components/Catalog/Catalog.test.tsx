f84f275c5f6c130eb2e63c214f7c528714634611
tikaya <tatiana.chefox@gmail.com>
2023-09-14 10:39:35 +0300

    imaging test

--- src/components/Catalog/Catalog.test.tsx
+++ src/components/Catalog/Catalog.test.tsx
@@ -23,3 +23,20 @@ test("getting pizza types", () => {
     expect(types).toBeInTheDocument();
     expect(types).toHaveTextContent("home.thickness.thinhome.thickness.traditional");
 })
+
+test("image acquisition", () => {
+    render(
+        <Provider store={store}>
+            <Catalog id={0}
+            title="Пепперони Фреш с перцем"
+            price={803}
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg"
+            count={1}
+            sizes={[26, 30, 40]}
+            types={[0, 1]} />
+        </Provider>
+    );
+
+    const image = screen.getByRole("img");
+    expect(image).toBeInTheDocument();
+})
