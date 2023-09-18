fbf0b79d7da7c83cf15a41299d59b79783e1bc1e
tikaya <tatiana.chefox@gmail.com>
2023-09-18 17:29:06 +0300

    deleting link

--- src/components/Catalog/Catalog.test.tsx
+++ src/components/Catalog/Catalog.test.tsx
@@ -94,3 +94,22 @@ test("pizza price display", () => {
     expect(price).toBeInTheDocument();
     expect(price).toHaveTextContent("803 ₽");
 })
+
+test("", () => {
+    render(
+        <Provider store={store}>
+            <Catalog id={0}
+            title="Пепперони Фреш с перцем"
+            price={803}
+            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg"
+            count={1}
+            sizes={[26, 30, 40]}
+            types={[0, 1]} />
+        </Provider>
+    );
+
+    const button = screen.getByRole("button");
+    const click = fireEvent.click(button);
+
+    expect(button).toBeInTheDocument();
+})
