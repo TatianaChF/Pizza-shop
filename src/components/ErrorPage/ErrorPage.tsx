3d20674b158eca0666033404a5aa21a006cd573e
tikaya <tatiana.chefox@gmail.com>
2023-09-18 21:55:23 +0300

    delete console.log

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -2,18 +2,17 @@ import { Link, useRouteError } from "react-router-dom";
 
 const ErrorPage = () => {
     const error = useRouteError();
-    console.error(error);
 
     return (
         <div>
             <h1>Ууупс!</h1>
-            <p>К сожалению, произошла непредвиденная ошибка.</p>
-            <p>
+            <p role="paragraph">К сожалению, произошла непредвиденная ошибка.</p>
+            <p role="error">
                 {(error as Error)?.message ||
                     (error as { statusText?: string })?.statusText
                 }
             </p>
-            <p>
+            <p role="link">
                 <Link to="/">На главную</Link>
             </p>
         </div>
