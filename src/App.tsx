56d113feaaac4cecaa744a3dbd2e34424dae8281
tikaya <tatiana.chefox@gmail.com>
2023-09-18 17:29:28 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.scss";
-import {Outlet} from "react-router-dom";
+import {BrowserRouter, Outlet} from "react-router-dom";
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Header from "./components/Header/Header";
