00f2e198f7d2952acada3b0b286b7bfbf3aafd28
tikaya <tatiana.chefox@gmail.com>
2023-09-25 21:56:01 +0300

    adding describe

--- src/components/Cart/Cart.test.ts
+++
@@ -1,9 +0,0 @@
-import { render } from "@testing-library/react";
-import {describe, expect, test} from '@jest/globals';
-import Cart from "./Cart";
-
-describe("Cart component", () => {
-    test("calculation of the total amount of the order", () => {
-        
-    })
-})
--- src/components/Cart/CartProduct/CartProduct.test.tsx
+++ src/components/Cart/CartProduct/CartProduct.test.tsx
@@ -5,82 +5,84 @@ import CartProduct from "./CartProduct";
 import { Provider } from 'react-redux';
 import { store } from '../../../redux/store';
 
-test("renders a title", () => {
-    render(
-        <Provider store={store}>
-           <CartProduct id={1} 
-            title="Сырная" 
-            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
-            price={245}
-            count={1}
-            size={26}
-            type="0" /> 
-        </Provider>
-    );
-    const titleElement = screen.getByText("home.title.Сырная");
-    expect(titleElement).toBeInTheDocument();
-});
-
-test("renders a count", () => {
-    render(
-        <Provider store={store}>
-            <CartProduct id={1} 
-            title="Сырная" 
-            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
-            price={245}
-            count={1}
-            size={26}
-            type="0" /> 
-        </Provider>
-    );
-    const countElement = screen.getByText("1");
-    expect(countElement).toBeInTheDocument();
-});
-
-test("renders a type and a size", () => {
-    render(
-        <Provider store={store}>
-            <CartProduct id={1} 
-            title="Сырная" 
-            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
-            price={245}
-            count={1}
-            size={26}
-            type="thin" /> 
-        </Provider>
-    );
-    const typeElement = screen.getByRole("p");
-    expect(typeElement).toHaveTextContent("home.thickness.thin, 26 home.abbreviations.sm");
-});
-
-test("price display", () => {
-    render(
-        <Provider store={store}>
-            <CartProduct id={1} 
-            title="Сырная" 
-            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
-            price={245}
-            count={1}
-            size={26}
-            type="thin" /> 
-        </Provider>
-    );
-    const priceElement = screen.getByText("245 ₽");
-    expect(priceElement).toBeInTheDocument();
-})
-
-test("multiplying the price and quantity of pizza", () => {
-    render(
-        <Provider store={store}>
-            <CartProduct id={1} 
-            title="Сырная" 
-            imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
-            price={245}
-            count={3}
-            size={26}
-            type="thin" /> 
-        </Provider>
-    );
-    const totalPricePizza = screen.getByRole("price");
-    expect(totalPricePizza).toHaveTextContent("735 ₽");
+describe("Cart component", () => {
+    test("renders a title", () => {
+        render(
+            <Provider store={store}>
+               <CartProduct id={1} 
+                title="Сырная" 
+                imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+                price={245}
+                count={1}
+                size={26}
+                type="0" /> 
+            </Provider>
+        );
+        const titleElement = screen.getByText("home.title.Сырная");
+        expect(titleElement).toBeInTheDocument();
+    });
+    
+    test("renders a count", () => {
+        render(
+            <Provider store={store}>
+                <CartProduct id={1} 
+                title="Сырная" 
+                imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+                price={245}
+                count={1}
+                size={26}
+                type="0" /> 
+            </Provider>
+        );
+        const countElement = screen.getByText("1");
+        expect(countElement).toBeInTheDocument();
+    });
+    
+    test("renders a type and a size", () => {
+        render(
+            <Provider store={store}>
+                <CartProduct id={1} 
+                title="Сырная" 
+                imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+                price={245}
+                count={1}
+                size={26}
+                type="thin" /> 
+            </Provider>
+        );
+        const typeElement = screen.getByRole("p");
+        expect(typeElement).toHaveTextContent("home.thickness.thin, 26 home.abbreviations.sm");
+    });
+    
+    test("price display", () => {
+        render(
+            <Provider store={store}>
+                <CartProduct id={1} 
+                title="Сырная" 
+                imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+                price={245}
+                count={1}
+                size={26}
+                type="thin" /> 
+            </Provider>
+        );
+        const priceElement = screen.getByText("245 ₽");
+        expect(priceElement).toBeInTheDocument();
+    });
+    
+    test("multiplying the price and quantity of pizza", () => {
+        render(
+            <Provider store={store}>
+                <CartProduct id={1} 
+                title="Сырная" 
+                imagePizza="https://dodopizza.azureedge.net/static/Img/Products/Pizza/ru-RU/2ffc31bb-132c-4c99-b894-53f7107a1441.jpg"
+                price={245}
+                count={3}
+                size={26}
+                type="thin" /> 
+            </Provider>
+        );
+        const totalPricePizza = screen.getByRole("price");
+        expect(totalPricePizza).toHaveTextContent("735 ₽");
+    })
 })
